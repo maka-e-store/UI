0a7a09fbf61b7abb7bfd65e0347f2b9170dec077
Michael Petillo <michaelpetillo2016@gmail.com>
2023-02-27 15:41:53 -0600

    done

--- maka-store/src/app/app.module.ts
+++ maka-store/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ maka-store/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+
+  ngOnInit(){
+    console.log('product-card');
+  }
+  constructor() {}
+
+}
